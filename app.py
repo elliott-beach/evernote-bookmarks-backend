c7caae71ebed767c6c255d737bbd7341cbf7bb0b
Elliott Beach <elliott2.71828@gmail.com>
2017-01-05 11:36:21 -0600

    set up heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from xml.sax.saxutils import escape
 
@@ -17,6 +18,9 @@ def index():
     res = "Evernote Note Creator!"
     return res
 
-app.run(port=3000)
+if __name__ == "__main__":
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(port=port)
 
 
