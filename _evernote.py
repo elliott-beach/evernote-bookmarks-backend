c96e4bead6c5ffc7756230fb8f711b0293d7549e
Elliott Beach <elliott2.71828@gmail.com>
2017-01-05 09:36:29 -0600

    change form to post request and catch errors

--- _evernote.py
+++ _evernote.py
@@ -1,5 +1,6 @@
 from evernote.api.client import EvernoteClient
 import evernote.edam.type.ttypes as Types
+from evernote.edam.error.ttypes import EDAMUserException
 
 import config
 
@@ -13,4 +14,16 @@ def createNote(title, content):
     note.title = title
     note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
     note.content += '<en-note>'+ content + '</en-note>'
-    return noteStore.createNote(note)
+    print content
+    try:
+        return noteStore.createNote(note)
+    except EDAMUserException as e:
+        raise
+
+# test
+if __name__ == "__main__":
+    title = "test"
+    #content = '<a href="https://console.aws.amazon.com/s3/home?region=us-west-2#&bucket=big-bucket-of-fun&prefix=">'
+    content='<a href="https://url.with.ampersand/foo?boof&amp;bar">FooBar</a>'
+    createNote(title, content)
+
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
+from xml.sax.saxutils import escape
 
 import _evernote as evernote
 
 app = Flask(__name__)
 
-@app.route('/create')
+@app.route('/create', methods=['POST'])
 def create():
-    title = request.args.get('title')
-    content = request.args.get('content')
+    title = request.form.get('title').encode('utf-8')
+    content = request.form.get('content').encode('utf-8')
     note = evernote.createNote(title, content)
     return str(note)
 
