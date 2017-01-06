1fdfc62dc88128d17bfb05d67f8b8521f4d40b81
Elliott Beach <elliott2.71828@gmail.com>
2017-01-06 15:35:14 -0600

    add CORS support

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
+import config
 from flask import Flask, request
 from xml.sax.saxutils import escape
+from flask_cors import cross_origin
 
 import _evernote as evernote
 
 app = Flask(__name__)
 
 @app.route('/create', methods=['POST'])
+@cross_origin(origin=config.ALLOWED_ORIGIN)
 def create():
     title = request.form.get('title').encode('utf-8')
     content = request.form.get('content').encode('utf-8')
@@ -22,5 +25,3 @@ if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
 	app.run(port=port)
-
-
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 import os
 dev_token=os.getenv('dev_token')
+ALLOWED_ORIGIN=os.getenv('origin')
