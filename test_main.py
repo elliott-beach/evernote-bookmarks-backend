a9f253f5808611a1562523336f1df1740ce28452
Elliott Beach <elliott2.71828@gmail.com>
2017-01-07 15:23:36 -0600

    get tests to pass

--- test_main.py
+++ test_main.py
@@ -7,10 +7,10 @@ port = int(os.environ.get('PORT', 5000))
 HOST = 'http://localhost:%d' % port
 
 # start the server!
-app.run(port=port)
+#app.run(port=port)
 
 def test_create():
-	response = requests.post(HOST+'/create', json={
+	response = requests.post(HOST+'/create', data={
 		'title': 'title',
 		'content': 'some example content'
 	})
