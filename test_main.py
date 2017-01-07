b7e9dba8a69562d1d80fc3d7467e61660740af51
Elliott Beach <elliott2.71828@gmail.com>
2017-01-07 14:10:38 -0600

    add travis tests -- APP WONT RUN

---
+++ test_main.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+from app import app
+
+port = int(os.environ.get('PORT', 5000))
+HOST = 'http://localhost:%d' % port
+
+# start the server!
+app.run(port=port)
+
+def test_create():
+	response = requests.post(HOST+'/create', json={
+		'title': 'title',
+		'content': 'some example content'
+	})
+	assert response.status_code == 200
