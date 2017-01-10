6001531f24b9fcf58c42dca5c1bcfc8cdb64e90a
Elliott Beach <elliott2.71828@gmail.com>
2017-01-10 10:25:41 -0600

    WIP add authentication

--- app.py
+++ app.py
@@ -1,12 +1,62 @@
 import os
 import config
-from flask import Flask, request
-from xml.sax.saxutils import escape
+from flask import Flask, request, session, redirect
 from flask_cors import cross_origin
+
+
+from evernote.api.client import EvernoteClient
+def get_evernote_client(token=None):
+    if token:
+        return EvernoteClient(token=token, sandbox=True)
+    else:
+        return EvernoteClient(
+            consumer_key=config.consumer_key,
+            consumer_secret=config.consumer_secret,
+            sandbox=True
+        )
 
 import _evernote as evernote
 
 app = Flask(__name__)
+
+# encrypt our sessions with the secret
+app.secret_key = config.secret_key
+
+@app.route('/')
+def index():
+    res = "Evernote Note Creator!"
+    return res
+
+### <Authentication> ###
+
+@app.route('/auth')
+def auth():
+    callbackUrl = config.host + '/auth_callback'
+    client = get_evernote_client()
+    request_token = client.get_request_token(callbackUrl)
+    # Save the request token information for later
+    session['oauth_token'] = request_token['oauth_token']
+    session['oauth_token_secret'] = request_token['oauth_token_secret']
+    # Redirect the user to the Evernote authorization URL
+    return redirect(client.get_authorize_url(request_token))
+
+@app.route('/auth_callback')
+def callback():
+    try:
+        client = get_evernote_client()
+        client.get_access_token(
+            session['oauth_token'],
+            session['oauth_token_secret'],
+            # The oauth_verifier will == None if user declined to authorize.
+            request.args.get('oauth_verifier', '')
+        )
+    except KeyError:
+        return redirect('/')
+    note_store = client.get_note_store()
+    notebooks = note_store.listNotebooks()
+    return 'Notebooks: %s' % str(notebooks)
+
+### <\Authentication> ###
 
 @app.route('/create', methods=['POST'])
 @cross_origin(origin=config.ALLOWED_ORIGIN)
@@ -15,11 +65,6 @@ def create():
     content = request.form.get('content').encode('utf-8')
     note = evernote.createNote(title, content)
     return str(note)
-
-@app.route('/')
-def index():
-    res = "Evernote Note Creator!"
-    return res
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 from os import environ as environment
 
+# @TODO set __dict__ = environ.__dict__
 dev_token = environment['dev_token']
 ALLOWED_ORIGIN = environment['origin']
+consumer_key = environment['consumer_key']
+consumer_secret = environment['consumer_secret']
+
+host = 'http://localhost:5000'
+secret_key = '@TODO'
