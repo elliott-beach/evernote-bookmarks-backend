fb455f866cdf3c4f041c1f9c284eb071bdef0f4b
Elliott Beach <elliott2.71828@gmail.com>
2017-01-19 20:54:49 -0600

    test creating note with notebook

--- test_main.py
+++ test_main.py
@@ -5,10 +5,17 @@ import os
 
 # Run tests in Evernote sandbox, not production Evernote. This also us to use the dev_token.
 config.sandbox = True
+token = config.dev_token
 
 # @TODO look at how Nithin Murali did his tests and write ours in the same way.
+# TODO move to environment
 port = int(os.environ.get('PORT', 5000))
 HOST = 'http://localhost:%d' % port
+
+# from http://stackoverflow.com/questions/2030053/random-strings-in-python
+import random, string
+def randomword(length):
+   return ''.join(random.choice(string.lowercase) for i in range(length))
 
 def test_home():
 	response = requests.get(HOST)
@@ -29,3 +36,10 @@ def test_create_notebook():
     except evernote.EDAMUserException as e:
         # 10 is anticipated error code if note has already been created.
         assert e.errorCode == 10
+
+def test_create_note_with_notebook():
+    # Danger -- could result in Exception if we generate a name that was generated before.
+    notebook_name = randomword(30)
+    uid = evernote.create_notebook(notebook_name, token)
+    note = evernote.create_note_with_notebook("New Note!", "Test", uid, token)
+    assert note
