250a859f09af55b6237d643bd376d73ba73580f7
Elliott Beach <elliott2.71828@gmail.com>
2017-02-06 22:54:06 -0600

    refactor tests

--- tests/online_test.py
+++ tests/online_test.py
@@ -7,9 +7,7 @@ import sys
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from backend import config, _evernote as evernote
-
-# Run tests in Evernote sandbox, not production Evernote. This also us to use the dev_token.
-config.sandbox = True
+config.sandbox=True
 token = config.dev_token
 
 # @TODO look at how Nithin Murali did his tests and write ours in the same way.
@@ -46,21 +44,33 @@ class TestServer(object):
     #     })
     #     assert response.status_code == 403
 
+def check_create_notebook():
+    try:
+        evernote.create_notebook("bookmarks", config.dev_token)
+    except evernote.EDAMSystemException as e:
+        # 19 code occurs when rate limit is breached, which can sometimes happen on sandbox for some reason?
+        assert e.errorCode == 19
+        raise
+    except evernote.EDAMUserException as e:
+        # 10 is anticipated error code if note has already been created
+        assert e.errorCode == 10
+
+
 class TestAPIClient(object):
 
+    def setup_class(self):
+        check_create_notebook()
+
+    def teardown_class(self):
+        uid = evernote.get_notebook("bookmarks", token).guid
+        evernote.get_client(token).get_note_store().expungeNotebook(uid)
+
+
     def test_get_notebook(self):
-        try:
-            evernote.create_notebook("bookmarks", config.dev_token)
-        except evernote.EDAMSystemException: # notebook already existed
-            raise
         notebook = evernote.get_notebook("bookmarks", config.dev_token)
         assert notebook.name == "bookmarks"
 
     def test_notebook_case(self):
-        try:
-            evernote.create_notebook("bookmarks", config.dev_token)
-        except evernote.EDAMSystemException: # notebook already existed
-            pass
         assert evernote.get_notebook("bookmarks", config.dev_token).guid == evernote.get_notebook("Bookmarks", config.dev_token).guid
 
     def test_get_notebook_err(self):
@@ -71,16 +81,6 @@ class TestAPIClient(object):
         else:
             assert False
 
-    def test_create_notebook(self):
-        # Deleting a Notebook is impossible with the API, so I cannot handle the case in which
-        # The Notebook has already been created.
-        try:
-            evernote.create_notebook("bookmarks", config.dev_token)
-        except evernote.EDAMSystemException as e:
-            # 19 is anticipated error code if note has already been created.
-            assert e.errorCode == 19
-
-
     def test_create_note_with_notebook(self):
         # Danger -- could result in Exception if we generate a name that was generated before.
         notebook_name = randomword(30)
@@ -88,7 +88,4 @@ class TestAPIClient(object):
         note = evernote.create_note_with_notebook("New Note!", "Test", uid, token)
         assert note
 
-    def test_API_expunge_notebook(self):
-        uid = evernote.get_notebook("bookmarks", token).guid
-        evernote.get_client(token).get_note_store().expungeNotebook(uid)
 
